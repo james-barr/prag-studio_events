1ee6752cad577776bd54c1215d241c5d3d80da47
James <jamesbarr711@gmail.com>
2017-08-14 16:48:46 -0600

    attempt to correct paperclip not using S3 properly in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,12 +92,13 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :url => ':s3_domain_url',
-    :path => '/:class/:attachmenet/:id_partition/:style/:filename',
+    :path => '/:class/:attachment/:id_partition/:style/:filename',
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
 
 end
